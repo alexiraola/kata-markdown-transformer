d9e0540457f4981be07b269fdc0adefce9235870
Alex Iraola <alex.iraola@gmail.com>
2024-01-20 16:52:59 +0100

    Move to its own file

--- src/tests/linkfinder.test.ts
+++ src/tests/linkfinder.test.ts
@@ -1,15 +1,4 @@
-class LinkFinder {
-  findLinks(markdown: string) {
-    const regex = /\[([^\[]+)\](\(.*\))/gm;
-    const singleRegex = /^\[([\w\s\d]+)\]\(((?:\/|https?:\/\/)[\w\d./?=#]+)\)$/;
-
-    const matches = markdown.match(regex);
-
-    return matches && matches.map(match => match.match(singleRegex))
-      .flatMap(match => match !== null ? [match] : [])
-      .map(([_, text, link]) => ({ text, link })) || [];
-  }
-}
+import { LinkFinder } from '../core/linkfinder';
 
 describe('Link finder', () => {
   let linkFinder: LinkFinder;

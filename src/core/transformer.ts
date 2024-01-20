c2cb35beb175d497105f9b8f5c75fa787a112dc7
Alex Iraola <alex.iraola@gmail.com>
2024-01-20 18:04:39 +0100

    Refactor: Remove LinkFinder

--- src/core/transformer.ts
+++ src/core/transformer.ts
@@ -1,22 +1,32 @@
-import { LinkFinder } from "../core/linkfinder";
-
+type Link = {
+  link: string,
+  text: string,
+  url: string
+}
 export class MarkdownTransformer {
 
   transform(markdown: string) {
+    const links = this.findLinks(markdown);
+    const uniqueLinks = Array.of(...new Set(links.map(link => link.url)));
+
+    const transformed = this.transformLinks(markdown, links, uniqueLinks);
+    const footnotes = this.generateFootnotes(uniqueLinks);
+
+    return [transformed, ...footnotes].join("\n");
+  }
+
+  private findLinks(markdown: string): Link[] {
     const regex = /\[([^\]]+)\]\(([^\)]+)\)/g;
-
-    const linkFinder = new LinkFinder();
-    const links = linkFinder.findLinks(markdown);
-    const uniqueLinks = Array.of(...new Set(links.map(link => link.link)));
-
     const matches = Array.from(markdown.matchAll(regex));
 
-    const transformed = matches.reduce((markdown, [link, text, url]) => {
+    return matches.map(([link, text, url]) => ({ link, text, url }));
+  }
+
+  private transformLinks(markdown: string, links: Link[], uniqueLinks: string[]) {
+    return links.reduce((markdown, { link, text, url }) => {
       const newLink = this.transformLink(text, url, uniqueLinks);
       return markdown.replace(link, newLink);
     }, markdown);
-
-    return [transformed, ...this.generateFootnotes(uniqueLinks)].join("\n");
   }
 
   private transformLink(text: string, url: string, uniqueLinks: string[]) {
